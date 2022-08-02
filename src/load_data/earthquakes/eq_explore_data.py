7807919b3ae9904694333330f67540c1ec4f7231
VarmiloVA <varmilo.blue@protonmail.com>
2022-08-02 18:18:28 +0200

    World map with earthquakes latitude and longitude implemented

--- src/load_data/earthquakes/eq_explore_data.py
+++ src/load_data/earthquakes/eq_explore_data.py
@@ -1,5 +1,5 @@
 import json
-from plotly.graph_objs import Bar, Layout
+from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 filename = 'data/eq_data_1_day_m1.json'
@@ -11,19 +11,19 @@ with open(filename, 'r') as f:
 # with open(readable_file, 'w') as f:
 #     json.dump(all_eq_data, f, indent=4)
 
-metadata = all_eq_data["metadata"]
 earthquakes = all_eq_data["features"]
-mags_recorded = []
-locations_recorded = []
+mags, lons, lats = [], [], []
 for earthquake in earthquakes:
-    properties = earthquake["properties"]
-    mags_recorded.append(properties["mag"])
-    locations_recorded.append(properties["place"])
+    mag = earthquake["properties"]["mag"]
+    lon = earthquake["geometry"]["coordinates"][0]
+    lat = earthquake["geometry"]["coordinates"][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
-# sorted_mags = sorted(mags_recorded)
-data = [Bar(x=locations_recorded, y=mags_recorded)]
-x_axis_config = {'title': 'Locations'}
-y_axis_config = {'title': 'Magnitude', 'dtick': 0.3}
-my_layout = Layout(title=str(metadata["title"]), xaxis=x_axis_config,
-    yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='graphs/earthquakes.html')
+# Locates the earthquakes in a world map
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='graphs/global_earthquakes.html')
