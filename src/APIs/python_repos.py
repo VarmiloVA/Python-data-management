63708948970c4c18d78232c37b4419d70355d952
VarmiloVA <varmilo.blue@protonmail.com>
2022-08-07 07:05:17 +0200

    An archive that gives info about the most starred python repos in github

---
+++ src/APIs/python_repos.py
@@ -0,0 +1,24 @@
+import requests
+
+# Does a request to the API
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'aplication/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Saves the answer given by the API in a variable
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Process the answer
+repo_dicts = response_dict['items']
+
+# Explores the most starred repositories
+print("\nSelected info about the top 10 most starred repo:")
+for repo_dict in repo_dicts[0:10]:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"\nOwner: {repo_dict['owner']['login']}")
+    print(f"\nStars: {repo_dict['stargazers_count']}")
+    print(f"\nDescription: {repo_dict['description']}")
+    print(f"\nForks: {repo_dict['forks_count']}")
+    print("\n")
