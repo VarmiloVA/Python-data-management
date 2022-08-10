1bc8a7a8efff3babbdb8a4aad083ba99d860fab3
VarmiloVA <varmilo.blue@protonmail.com>
2022-08-11 01:36:41 +0200

    Comment with the principal hacker-news articles implemented

--- src/APIs/hn_article.py
+++ src/APIs/hn_article.py
@@ -3,6 +3,7 @@ import json
 
 # Does a request to the API and saves the answer
 url = "https://hacker-news.firebaseio.com/v0/item/19155826.json"
+# Principal hacker-news articles: https://hacker-news.firebaseio.com/v0/topstories.json
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
